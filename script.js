1faeb581768a2b5e25678358e393ba0ffb2517c5
Szymon-Levy <94991990+Szymon-Levy@users.noreply.github.com>
2023-01-07 22:44:40 +0100

    .

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ offcanvasToggler.addEventListener("click", () =>{
     navigation.classList.toggle("active");
     offcanvasToggler.classList.toggle("active");
 });
+
+
+//images slider
+
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 10,
+    slidesPerView: 2,
+    grabCursor: true,
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+        },
+    },
+
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+
+  });
